30e22b047f876ae950e858d00b5123ee3d0e362c
flobbe9 <florin735@live.com>
2023-09-15 22:09:59 +0200

    init

---
+++ src/test/java/com/example/vorspiel_userservice/VorspielUserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vorspiel_userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VorspielUserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
